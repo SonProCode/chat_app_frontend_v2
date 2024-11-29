330402d4fe66df45c9e25047787c042b90ac3ebe
SonProCode <sonchl20182021@gmail.com>
2024-11-30 01:35:02 +0700

    sua loi goi 2 dau

--- src/server/hooks/useAnswerProcessing.tsx
+++ src/server/hooks/useAnswerProcessing.tsx
@@ -1,13 +1,19 @@
 import { useCallback } from "react";
+import { useLocalCameraStream } from "./useLocalCameraStream";
 
 export function useAnswerProcessing(peerConnection: RTCPeerConnection) {
+  const { localStream } = useLocalCameraStream();
   const handleOfferAnswer = useCallback(
     ({ answer }: { answer: RTCSessionDescriptionInit }) => {
+      localStream?.getTracks().forEach((track) => {
+        //console.log("TRACK", track);
+        peerConnection.addTrack(track, localStream);
+      });
       // neu ko chahy duoc thi cop ben usePeerConnection vao
       //console.log("ANSWER", answer);
       peerConnection.setRemoteDescription(answer);
     },
-    [peerConnection],
+    [localStream, peerConnection],
   );
 
   return {
--- src/server/hooks/useSendingAnswer.tsx
+++ src/server/hooks/useSendingAnswer.tsx
@@ -1,20 +1,41 @@
 import { useSocket } from "@/provider/socketProvider";
 import { SOCKET_EVENTS } from "@/utils/constant";
 import { useCallback } from "react";
-
 import { useParams } from "react-router-dom";
 
 export function useSendingAnswer(peerConnection: RTCPeerConnection) {
   const { id } = useParams();
   const socket = useSocket();
+
   const handleConnectionOffer = useCallback(
     async ({ offer }: { offer: RTCSessionDescriptionInit }) => {
-      await peerConnection.setRemoteDescription(offer);
-      const answer = await peerConnection.createAnswer();
-      //console.log("ANSWER IN useSendingAnswer", answer);
-      await peerConnection.setLocalDescription(answer);
+      try {
+        if (peerConnection.signalingState !== "stable") {
+          console.warn(
+            "PeerConnection is not in a stable state:",
+            peerConnection.signalingState,
+          );
+          return;
+        }
 
-      socket?.emit(SOCKET_EVENTS.CLIENT.ANSWER, { answer, roomName: id });
+        await peerConnection.setRemoteDescription(offer);
+
+        if (peerConnection.signalingState === "have-remote-offer") {
+          const answer = await peerConnection.createAnswer();
+          console.log("ANSWER IN useSendingAnswer", answer);
+
+          await peerConnection.setLocalDescription(answer);
+
+          socket?.emit(SOCKET_EVENTS.CLIENT.ANSWER, { answer, roomName: id });
+        } else {
+          console.warn(
+            "Unexpected signaling state after setRemoteDescription:",
+            peerConnection.signalingState,
+          );
+        }
+      } catch (error) {
+        console.error("Error in handling connection offer:", error);
+      }
     },
     [id, peerConnection, socket],
   );
