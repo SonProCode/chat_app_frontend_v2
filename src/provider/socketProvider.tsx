5776261cfb79fb73d163eeb0234d4757507a113a
SonProCode <sonchl20182021@gmail.com>
2024-12-07 01:24:39 +0700

    fix socket

--- src/provider/socketProvider.tsx
+++ src/provider/socketProvider.tsx
@@ -18,14 +18,13 @@ export const useSocket = () => {
 export const SocketProvider = (props: { children: ReactNode }): JSX.Element => {
   const { children } = props;
   const [socket, setSocket] = useState<Socket>();
-
   useEffect(() => {
     // Lấy token từ localStorage (hoặc bất kỳ nơi nào bạn lưu trữ token)
     const token = localStorage.getItem("token"); // Giả sử token được lưu trữ trong localStorage
 
     if (token) {
       // Nếu token có, thêm nó vào query khi kết nối với WebSocket
-      const newSocket = io("http://localhost:3000", {
+      const newSocket = io("https://chat-app-frontend-v2-9xdq.vercel.app/", {
         transports: ["websocket"],
         query: { token }, // Thêm token vào query
         reconnectionAttempts: 5, // Number of retry attempts
