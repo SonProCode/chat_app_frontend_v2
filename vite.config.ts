4c1da8814c40e8e21ba513411734c8f65e67ecd9
SonProCode <sonchl20182021@gmail.com>
2024-12-09 14:20:41 +0700

    oke

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server: { host: true, strictPort: true, port: 4173 },
 });
